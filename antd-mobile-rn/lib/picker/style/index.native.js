b2fff4d7a70b001a212f3984269e2589e52d79ab
zhoule <619254379@qq.com>
2019-03-22 14:33:23 +0800

    first commit

---
+++ antd-mobile-rn/lib/picker/style/index.native.js
@@ -0,0 +1,68 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+exports['default'] = {
+    modal: {
+        flex: 1,
+        flexDirection: 'column',
+        justifyContent: 'flex-end'
+    },
+    header: {
+        flexGrow: 1,
+        height: 58,
+        // alignItems: 'center',
+        flexDirection: 'row',
+        // justifyContent: 'center',
+        justifyContent: 'space-around',
+        borderWidth: 1,
+        borderColor: '#E5DEFD'
+    },
+    headerItem: {
+        height: 58,
+        width: 94,
+        // flex: 1,
+        // alignItems: 'center',
+        justifyContent: 'center'
+    },
+    actionText: {
+        // color: '#0ae',
+        fontSize: 18,
+        textAlign: 'center'
+    },
+    okText: {
+        width: 94,
+        height: 40,
+        lineHeight: 38,
+        color: '#7545E2',
+        fontSize: 22,
+        textAlign: 'center',
+        borderColor: '#7545E2',
+        borderWidth: 1,
+        borderRadius: 20,
+    },
+    dismissText: {
+        width: 94,
+        height: 40,
+        lineHeight: 38,
+        color: '#7545E2',
+        fontSize: 22,
+        textAlign: 'center',
+        borderColor: '#7545E2',
+        borderWidth: 1,
+        borderRadius: 20,
+    },
+    title: {
+        // width: 94,
+        // height: 40,
+        // lineHeight: 38,
+        // color: '#7545E2',
+        // fontSize: 22,
+        // textAlign: 'center',
+        // borderColor: '#7545E2',
+        // borderWidth: 1,
+        // borderRadius: 20,
+    }
+};
+module.exports = exports['default'];
